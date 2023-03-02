5c2e59cde344c24240c01961f02f9dd9f9b2780f
Bice93 <bianca.cerulo@hotmail.it>
2023-03-02 14:49:50 +0100

    Created photo reading methods

--- src/main/java/com/corsojava/fotoalbum/controller/PhotoController.java
+++ src/main/java/com/corsojava/fotoalbum/controller/PhotoController.java
@@ -1,10 +1,43 @@
 package com.corsojava.fotoalbum.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.corsojava.fotoalbum.model.Photo;
+import com.corsojava.fotoalbum.repository.PhotoRepository;
 
 @Controller
 @RequestMapping("/photos")
 public class PhotoController {
-
+	
+	private @Autowired PhotoRepository photoR;
+	
+	@GetMapping
+	public String index(@RequestParam(name="search", required = false) String keyword, Model model) {
+		List<Photo> photoList;
+		if(keyword != null && !keyword.isEmpty()) {
+			photoList = photoR.findByTitleLikeOrTagLike("%"+keyword+"%", "%"+keyword+"%");
+		} else {
+			photoList = photoR.findAll();
+		}
+		model.addAttribute("photos", photoList);
+		
+		return "/photos/index";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") Long id, Model model) {
+		Optional<Photo> photo = photoR.findById(id);
+		model.addAttribute("photo", photo.get());
+		
+		return "/photos/show";
+	}
 }
--- src/main/java/com/corsojava/fotoalbum/repository/PhotoRepository.java
+++ src/main/java/com/corsojava/fotoalbum/repository/PhotoRepository.java
@@ -1,9 +1,12 @@
 package com.corsojava.fotoalbum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.corsojava.fotoalbum.model.Photo;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 	//ha già tutti metodi per eseguire le operazioni CRUD
+	public List<Photo> findByTitleLikeOrTagLike(String title, String tag);
 }
